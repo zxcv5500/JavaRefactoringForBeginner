78e1830ae9ba93453eb7506cdc0a9f06a0a34505
zxcv5500 <zxcv5500@gmail.com>
2018-05-08 16:52:38 +0900

    분류 코드를 Enum으로 치환한 코드 예시

---
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithClass/Enum/ItemType.java
@@ -0,0 +1,15 @@
+package ReplaceTypeCodeWithClass.Enum;
+
+public enum ItemType {
+	BOOK(0),
+	DVD(1),
+	SOFTWARE(2);
+	
+	private final int _typecode;
+	
+	private ItemType(int typecode) {
+		_typecode = typecode;
+	}
+	
+	public int getTypecode() { return _typecode; }
+}
---
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithClass/Enum/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author zxcv5500
+ * 분류 코드를 enum으로 치환한 코드
+ */
+package ReplaceTypeCodeWithClass.Enum;
