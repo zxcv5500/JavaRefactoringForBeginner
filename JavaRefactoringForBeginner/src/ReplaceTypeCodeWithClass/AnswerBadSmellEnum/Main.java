619d43727ea2e2b11cb5879a605cb554a2c798a8
zxcv5500 <zxcv5500@gmail.com>
2018-05-08 20:03:53 +0900

    분류코드 enum으로 치환 후

--- JavaRefactoringForBeginner/src/ReplaceTypeCodeWithClass/AnswerBadSmellEnum/Main.java
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithClass/AnswerBadSmellEnum/Main.java
@@ -3,8 +3,8 @@ package ReplaceTypeCodeWithClass.AnswerBadSmellEnum;
 public class Main {
 
 	public static void main(String[] args) {
-		LunchSet lunch_a_juice = new LunchSet(LunchSet.LUNCH_TYPE_A, LunchSet.DRINK_TYPE_JUICE);
-		LunchSet lunch_b_coffee = new LunchSet(LunchSet.LUNCH_TYPE_B, LunchSet.DRINK_TYPE_COFFEE);
+		LunchSet lunch_a_juice = new LunchSet(LunchType.A, DrinkType.JUICE);
+		LunchSet lunch_b_coffee = new LunchSet(LunchType.B, DrinkType.COFFEE);
 		
 		System.out.println(lunch_a_juice);
 		System.out.println(lunch_b_coffee);
