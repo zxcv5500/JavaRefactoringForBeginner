bb3cc76e829c6eaf659a0d4e45b5fbf82ff81a35
zxcv5500 <zxcv5500@gmail.com>
2018-05-08 09:04:42 +0900

    패키지 정보 주석 추가.

--- JavaRefactoringForBeginner/src/ReplaceTypeCodeWithClass/before/package-info.java
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithClass/before/package-info.java
@@ -3,6 +3,6 @@
  */
 /**
  * @author zxcv5500
- *
+ * 분류 코드를 클래스로 치환하기 리팩토링 전의 최초 단계
  */
 package ReplaceTypeCodeWithClass.before;
---
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithClass/step1/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author zxcv5500
+ * 분류 코드를 클래스로 치환하기 step1 
+ */
+package ReplaceTypeCodeWithClass.step1;
