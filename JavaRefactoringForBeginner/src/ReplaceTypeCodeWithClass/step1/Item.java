e4605f1d3f43194bf85a5c4ceb0e9640d0207eff
zxcv5500 <zxcv5500@gmail.com>
2018-05-08 11:41:40 +0900

    분류 코드를 나타내는 새로운 클래스 작성 및 적용

--- JavaRefactoringForBeginner/src/ReplaceTypeCodeWithClass/step1/Item.java
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithClass/step1/Item.java
@@ -1,21 +1,32 @@
 package ReplaceTypeCodeWithClass.step1;
 
 public class Item {
+	/* #1 분류 코드를 ItemType클래스로 대체함 
 	public static final int TYPECODE_BOOK = 0;
 	public static final int TYPECODE_DVD = 1;
 	public static final int TYPECODE_SOFTWARE = 2;
+	//*/
 	
-	private final int _typecode;
+	public static final int TYPECODE_BOOK = ItemType.BOOK.getTypecode();
+	public static final int TYPECODE_DVD = ItemType.DVD.getTypecode();
+	public static final int TYPECODE_SOFTWARE = ItemType.SOFTWARE.getTypecode();
+	
+//	private final int _typecode;  //#2 분류 코드를 ItemType클래스를 사용하도록 변경
+	private final ItemType _itemtype;
 	private final String _title;
 	private final int _price;
+
+	
 	
 	public Item(int typecode, String title, int price) {
-		_typecode = typecode;
+//		_typecode = typecode; //#3 분류코드에서 ItemType 인스턴스로 변환
+		_itemtype = ItemType.getItemType(typecode);
 		_title = title;
 		_price = price;
 	}
 	
-	public int getTypecode() { return _typecode; }
+//	public int getTypecode() { return _typecode; } // #4 ItemType을 사용하도록 수정
+	public int getTypecode() { return _itemtype.getTypecode(); }
 	public String getTitle() { return _title; }
 	public int getPrice() { return _price; }
 	
@@ -26,4 +37,5 @@ public class Item {
 				+ getPrice() + " ]";
 	}
 	
+	// ....
 }
