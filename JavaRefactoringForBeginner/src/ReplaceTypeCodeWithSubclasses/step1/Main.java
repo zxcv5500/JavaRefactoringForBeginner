f7d086bc9f41695858eb0f3ea89d19cc029164aa
zxcv5500 <zxcv5500@gmail.com>
2018-05-09 16:56:36 +0900

    new 대신에 createShape팩토리 메소드를 사용하도록 수정

--- JavaRefactoringForBeginner/src/ReplaceTypeCodeWithSubclasses/step1/Main.java
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithSubclasses/step1/Main.java
@@ -2,10 +2,11 @@ package ReplaceTypeCodeWithSubclasses.step1;
 
 public class Main {
 	public static void main(String args[]) {
-		Shape line = new Shape(Shape.TYPECODE_LINE, 0, 0, 100, 200);
-		Shape rectangle = new Shape(Shape.TYPECODE_RECTANGLE, 10, 20, 30, 40);
-		Shape oval = new Shape(Shape.TYPECODE_OVAL, 100, 200, 300, 400);
-		
+		//* #1 new 키워드로 물건을 생성하던 걸 팩토리 메소드로 치환한다.
+		Shape line = Shape.createShape(Shape.TYPECODE_LINE, 0, 0, 100, 200);
+		Shape rectangle =Shape.createShape(Shape.TYPECODE_RECTANGLE, 10, 20, 30, 40);
+		Shape oval = Shape.createShape(Shape.TYPECODE_OVAL, 100, 200, 300, 400);
+		//*/
 		Shape[] shape = {
 				line, rectangle, oval
 		};
