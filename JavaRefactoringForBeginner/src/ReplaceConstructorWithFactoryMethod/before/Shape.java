eb6da1158d4b4ce32be35ca4de007cb96ecceb60
zxcv5500 <zxcv5500@gmail.com>
2018-05-10 21:29:23 +0900

    생성자를 팩토리 메서드로 치환 리팩토링 전 코드 작성

---
+++ JavaRefactoringForBeginner/src/ReplaceConstructorWithFactoryMethod/before/Shape.java
@@ -0,0 +1,67 @@
+package ReplaceConstructorWithFactoryMethod.before;
+
+public class Shape {
+	public static final int TYPECODE_LINE = 0;
+	public static final int TYPECODE_RECTANGLE = 1;
+	public static final int TYPECODE_OVAL = 2;
+	
+	private final int _typecode;
+	private final int _startx; 
+	private final int _starty;
+	private final int _endx;
+	private final int _endy;
+	
+	public Shape(int typecode, int startx, int starty, int endx, int endy) {
+		_typecode = typecode;
+		_startx = startx;
+		_starty = starty;
+		_endx = endx;
+		_endy = endy;
+	}
+	
+	public int getTypecode() {
+		return _typecode;
+	}
+	
+	public String getName() {
+		switch (_typecode) {
+		case TYPECODE_LINE:
+			return "LINE";
+		case TYPECODE_RECTANGLE:
+			return "RECTANGLE";
+		case TYPECODE_OVAL:
+			return "OVAL";
+		default:
+			return null;
+		}
+	}
+	
+	public void draw() {
+		switch(_typecode) {
+		case TYPECODE_LINE:
+			drawLine();
+			break;
+		case TYPECODE_RECTANGLE:
+			drawRectangle();
+			break;
+		case TYPECODE_OVAL:
+			drawOval();
+			break;
+		}
+	}
+	
+	private void drawLine() {
+		System.out.println("drawLine : " + this.toString());
+		// ...
+	}
+	
+	private void drawRectangle() {
+		System.out.println("drawRectangle : " + this.toString());
+		// ...
+	}
+	
+	private void drawOval() {
+		System.out.println("drawOval : " + this.toString());
+		// ...
+	}
+}
