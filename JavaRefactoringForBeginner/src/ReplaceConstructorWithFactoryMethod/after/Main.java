184c3f681bea80b6cf1bea8ba40c3121c669833d
zxcv5500 <zxcv5500@gmail.com>
2018-05-12 19:38:35 +0900

    before 패키지의 코드를 리팩토링 완료한 모습의 코드가 있는 패키지

---
+++ JavaRefactoringForBeginner/src/ReplaceConstructorWithFactoryMethod/after/Main.java
@@ -0,0 +1,19 @@
+package ReplaceConstructorWithFactoryMethod.after;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape line = Shape.create(Shape.TYPECODE_LINE, 0, 0, 100, 200);
+        Shape rectangle = Shape.create(Shape.TYPECODE_RECTANGLE, 10, 20, 30, 40);
+        Shape oval = Shape.create(Shape.TYPECODE_OVAL, 100, 200, 300, 400);
+
+        Shape[] shape = {
+            line,
+            rectangle,
+            oval,
+        };
+
+        for (Shape s : shape) {
+            s.draw();
+        }
+    }
+}
---
+++ JavaRefactoringForBeginner/src/ReplaceConstructorWithFactoryMethod/after/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author zxcv5500
+ * before 패키지의 코드를 리팩토링 완료한 모습의 코드가 있는 패키지
+ */
+package ReplaceConstructorWithFactoryMethod.after;
