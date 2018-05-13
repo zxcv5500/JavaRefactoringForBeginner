fc2685f36fb1ef00abc1c9f1f35267623688d457
zxcv5500 <zxcv5500@gmail.com>
2018-05-14 01:12:23 +0900

    before 패키지 코드 복붙함. 이 코드를 중심으로 수정 놀이를 연습함

---
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithSubclasses/playground/Shape.java
@@ -0,0 +1,78 @@
+package ReplaceTypeCodeWithSubclasses.playground;
+
+public class Shape {
+	public static final int TYPECODE_LINE = 0;
+	public static final int TYPECODE_RECTANGLE = 1;
+	public static final int TYPECODE_OVAL = 2;
+	
+	private final int _typecode;
+	private final int _startx;
+	private final int _starty;
+	private final int _endx;
+	private final int _endy;
+	
+
+	
+	protected Shape(int typecode, int startx, int starty, int endx, int endy) {
+		super();
+		this._typecode = typecode;
+		this._startx = startx;
+		this._starty = starty;
+		this._endx = endx;
+		this._endy = endy;
+	}
+	
+	public int getTypecode() { return _typecode; }
+	
+	public String getName() {
+		switch (getTypecode()) {
+		case TYPECODE_LINE:
+			return "LINE";
+		case TYPECODE_RECTANGLE:
+			return "RECTANGLE";
+		case TYPECODE_OVAL:
+			return "OVAL";
+		default:
+			return null;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "[ " 
+				+ getName() + ", "
+				+ "(" + _startx + ", " + _starty + ", "
+				+ "(" + _endx + ", " + _endy + ") ]";
+	}
+	
+	public void draw() {
+		switch (getTypecode()) {
+		case TYPECODE_LINE:
+			drawLine();
+			break;
+		case TYPECODE_RECTANGLE:
+			drawRectangle();
+			break;
+		case TYPECODE_OVAL:
+			drawOval();
+			break;
+		default:
+			;
+		}
+	}
+	
+	public void drawLine() {
+		System.out.println("drawLine : " + this.toString());
+		// ...
+	}
+	
+	public void drawRectangle() {
+		System.out.println("drawRectangle : " + this.toString());
+		// ...
+	}
+	
+	public void drawOval() {
+		System.out.println("drawOval ; " + this.toString());
+		// ...
+	}
+}
