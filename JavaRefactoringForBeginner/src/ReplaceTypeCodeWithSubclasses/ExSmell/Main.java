73b40423c97456f2dd956ccd46503213232b041b
zxcv5500 <zxcv5500@gmail.com>
2018-05-14 17:03:41 +0900

    분류 코드를 하위 클래스로 치환하기 연습문제 원본

---
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithSubclasses/ExSmell/Main.java
@@ -0,0 +1,15 @@
+package ReplaceTypeCodeWithSubclasses.ExSmell;
+
+public class Main {
+    public static void main(String[] args) {
+        Player[] players = {
+            new Player(Player.MUSIC),
+            new Player(Player.VIDEO),
+        };
+
+        for (Player player : players) {
+            player.play();
+            player.stop();
+        }
+    }
+}
---
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithSubclasses/ExSmell/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author zxcv5500
+ * 분류 코드를 하위 클래스로 치환하기 연습문제 패키지
+ */
+package ReplaceTypeCodeWithSubclasses.ExSmell;
