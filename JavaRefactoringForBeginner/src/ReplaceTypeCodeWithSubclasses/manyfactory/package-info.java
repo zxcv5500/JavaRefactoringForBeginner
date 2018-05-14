310ddc191624956af2efac0b7db478e232546c53
zxcv5500 <zxcv5500@gmail.com>
2018-05-14 11:20:19 +0900

    factory 패키지에서 다룬 내용 처럼 팩토리 클래스를 통해서 switch를 없애려고 하면 클래스가 너무 많아지고 코드 가독성이 떨어진다. 이 단점을 개선해 보는 패키지

---
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithSubclasses/manyfactory/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author zxcv5500
+ * factory 패키지에서 다룬 내용 처럼 팩토리 클래스를 통해서 switch를 없애려고 하면 클래스가 너무 많아지고 코드 가독성이 떨어진다.
+ * 이 단점을 개선해 보는 패키지 
+ */
+package ReplaceTypeCodeWithSubclasses.manyfactory;
