3aa4198f6fbb742f7b08da97bad4778adf9b9044
zxcv5500 <zxcv5500@gmail.com>
2018-05-15 00:57:52 +0900

    이 패키지의 연습문제 해답 패키지 코드 추가

---
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithSubclasses/answerSmell/Main.java
@@ -0,0 +1,15 @@
+package ReplaceTypeCodeWithSubclasses.answerSmell;
+
+public class Main {
+    public static void main(String[] args) {
+        Player[] players = {
+            Player.create(Player.MUSIC),
+            Player.create(Player.VIDEO),
+        };
+
+        for (Player player : players) {
+            player.play();
+            player.stop();
+        }
+    }
+}
