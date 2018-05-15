a2d942bd472bd714960d7090b27c31e705a3de1e
zxcv5500 <zxcv5500@gmail.com>
2018-05-15 09:36:13 +0900

    연습문제 해답 코드 업로드

---
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithStateStrategy/answerSmell/Main.java
@@ -0,0 +1,20 @@
+package ReplaceTypeCodeWithStateStrategy.answerSmell;
+
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player();
+
+        player.play();
+
+        player.setMedia(new Video());
+        player.play();
+        player.stop();
+
+        player.setMedia(new Music());
+        player.loop();
+        player.stop();
+
+        player.setMedia(null);
+    }
+}
+
