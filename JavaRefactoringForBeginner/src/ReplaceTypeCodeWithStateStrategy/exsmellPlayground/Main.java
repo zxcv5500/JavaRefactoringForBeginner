de1dc0487b7bc08c2795e22e69435f5ad012cabb
zxcv5500 <zxcv5500@gmail.com>
2018-05-15 09:52:21 +0900

    연습문제 원본 코드 업로드

---
+++ JavaRefactoringForBeginner/src/ReplaceTypeCodeWithStateStrategy/exsmellPlayground/Main.java
@@ -0,0 +1,19 @@
+package ReplaceTypeCodeWithStateStrategy.exsmellPlayground;
+
+public class Main {
+    public static void main(String[] args) {
+        Player player = new Player();
+
+        player.play();
+
+        player.setMedia(Media.VIDEO);
+        player.play();
+        player.stop();
+
+        player.setMedia(Media.MUSIC);
+        player.loop();
+        player.stop();
+
+        player.setMedia(Media.NULL);
+    }
+}
